01ef0340479cc4a03998ac9c76fa5da49281937a
DKHRAPTH <81453652+DKHRAPTH@users.noreply.github.com>
2025-11-01 09:36:34 +0700

    Fix ui display

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ from discord import app_commands
 from discord import FFmpegPCMAudio
 from dotenv import load_dotenv
 from yt_dlp import YoutubeDL
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix='=', intents=intents)
-
 @bot.event
 async def on_ready():
     await bot.tree.sync()
@@ -22,15 +19,15 @@ async def on_ready():
 @app_commands.describe(yt_url="ใส่ url เพลงที่ต้องการจะฟัง")
 async def play_yt(interaction:discord.Interaction, yt_url: str):
     if not interaction.user.voice:
-        await interaction.response.send_message("❌ เข้าห้องเสียงก่อน!", ephemeral=True)
+        await interaction.response.send_message("🤖 เข้าห้องเสียงก่อน! ❌", ephemeral=True)
         return
     else:
-        await interaction.user.voice.channel.connect()
-        await interaction.response.send_message(f'กำลังโหลดเพลง', ephemeral=True)
+        await interaction.user.voice.channel.connect(timeout=60)
+        await interaction.response.send_message(f'🤖 กำลังโหลดเพลง ⚪', ephemeral=True)
         audio,title = extrct_url(yt_url)
         vc = interaction.guild.voice_client
         vc.play(FFmpegPCMAudio(audio, options='-vn -b:a 128k -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'))
-        await interaction.followup.send(f"กำลังเล่นเพลง {title}")
+        await interaction.followup.send(f"🤖 กำลังเล่นเพลง {title}")
 @bot.tree.command(name="leave", description="Bot leave channel")
 async def leave(interaction:discord.Integration):
     await interaction.guild.voice_client.disconnect()
